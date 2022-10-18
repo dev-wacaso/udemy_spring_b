713c1ccd30678f76e21bda4878e06d065d1513d5
QJ-Dish <91288059+qj-dish@users.noreply.github.com>
2022-10-18 08:55:18 -0600

    added working web module before addition of Thymeleaf

--- guess/web/src/main/java/com/isquibly/WebMain.java
+++ guess/web/src/main/java/com/isquibly/WebMain.java
@@ -1,11 +1,14 @@
 package com.isquibly;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class WebMain {
     public static void main(String[] args) {
+        log.info("Starting WebMain()...");
         SpringApplication.run(WebMain.class, args);
     }
 }
